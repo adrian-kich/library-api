728ee09d6ce94365072ccba8e4509018eb903a82
adrian-kich <kich.adrian@gmail.com>
2024-11-25 12:03:11 -0300

    initial commit

---
+++ src/test/java/com/adriankich/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriankich.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
